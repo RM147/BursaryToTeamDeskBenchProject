dfa8f4963d95dced01c79da6da3fc8e1765dbe3c
JoshuaGomersall <47025499+JoshuaGomersall@users.noreply.github.com>
2019-06-06 14:48:03 +0100

    New Encrypting And Decryption Tests

---
+++ tests/demo_create_mongo_db.js
@@ -0,0 +1,12 @@
+var MongoClient = require('mongodb').MongoClient;
+//Create a database named "mydb":
+var url = "mongodb://localhost:27017/mydb";
+
+function make(){
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
+}
+module.exports = make;
---
+++ tests/demo_mongodb_find.js.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+function find (){
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo.collection("customers").find({}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+});
+}
+
+find();
+
+module.exports = find;
